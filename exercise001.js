2f67525675daba4df290eb0a6f972bf44acbe9c3
unknown <marlon_tancredo@hotmail.com>
2023-07-28 22:27:44 +0100

    feat: using then() catch() and async await to get reponse

--- exercise001.js
+++ exercise001.js
@@ -1,14 +1,32 @@
 const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+  // Generate random number between 0 and 9
+  const randomInt = Math.floor(Math.random() * 10);
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
-	}
+  if (randomInt % 2 === 0) {
+    // Success
+    setTimeout(() => resolve("done"), 2000);
+  } else {
+    // Failure
+    setTimeout(() => reject("error"), 2000);
+  }
 });
 
-// Your solution(s) to exercise001 here!
+//--Task 1-- 🔥
+// promise
+//   .then((response) => {
+//     console.log(`Yay! Promise resolved with response: ${response}`);
+//   })
+//   .catch((error) => {
+//     console.error(`Boo. Promise rejected with response: ${error}`);
+//   });
+
+//--Task 2-- 🔥
+const getResponse = async () => {
+  try {
+    const response = await promise;
+    console.log(`Yay! Promise resolved with response: ${response}`);
+  } catch (error) {
+    console.error(`Boo. Promise rejected with response: ${error}`);
+  }
+};
+getResponse();
