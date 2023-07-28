e8541a266f8e4a89441dfa4f649d57ffaf2753d1
unknown <marlon_tancredo@hotmail.com>
2023-07-28 22:43:26 +0100

    refactor: refactoring exercise002.js

--- exercise002.js
+++ exercise002.js
@@ -1,17 +1,7 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
-
-// const fetchData = (apiEndPoint) => {
-//   fetch(apiEndPoint)
-//     .then((response) => response.json())
-//     .then((json) => console.log(json))
-//     .catch((error) => console.log(error));
-// };
-
-// fetchData(jsonTypicode);
-
 // 🔥 -- Converting then() catch() to async await -- 🔥
+const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 const fetchData = async () => {
   try {
     const response = await fetch(jsonTypicode);
