137a1f9c1e7c07942eb062926c9191fd2728f523
unknown <marlon_tancredo@hotmail.com>
2023-07-28 22:39:28 +0100

    feat: implementing async await function into exercise002.js

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,24 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
+
+// fetchData(jsonTypicode);
+
+// 🔥 -- Converting then() catch() to async await -- 🔥
+const fetchData = async () => {
+  try {
+    const response = await fetch(jsonTypicode);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-fetchData(jsonTypicode);
+fetchData();
